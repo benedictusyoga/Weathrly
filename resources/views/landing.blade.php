ced2b85c1080787eac67124c2b0efe99436fa742
Benedictus Yogatama <yogatama4561@gmail.com>
2024-11-26 15:39:28 +0700

    navbar layout init

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,38 +8,11 @@
     <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ asset('css/landing.css') }}" rel="stylesheet">
 </head>
+
 <body>
     <div class="overlay"></div>
+    @include('layout.navbar')
     <div class="content">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
-            <div class="container">
-                <a class="navbar-brand" href="#">
-                    <img src="{{ asset('images/logo.png') }}" alt="Weathrly">
-                    <span>WEATHRLY</span>
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Report</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">News</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                John Doe
-                                <span class="ms-2">
-                                    <img src="{{ asset('images/logo.png') }}" alt="Profile Icon" class="rounded-circle profile-icon">
-                                </span>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
         <div class="main-box">
             <h1>Welcome to Weathrly</h1>
             <p>Your trusted source for weather updates.</p>
@@ -46,4 +20,5 @@
     </div>
     <script src="{{ asset('js/bootstrap.js') }}"></script>
 </body>
+
 </html>
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('images/logo.png') }}" alt="Weathrly">
+            <span>WEATHRLY</span>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Report</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        John Doe
+                        <span class="ms-2">
+                            <img src="{{ asset('images/profile.png') }}" alt="Profile Icon" class="rounded-circle profile-icon">
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
