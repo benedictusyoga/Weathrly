134db3285d44f70c41bb6a32ee44047272a419ea
Benedictus Yogatama <yogatama4561@gmail.com>
2024-12-04 13:11:58 +0700

    Reapply "Merge pull request #18 from benedictusyoga/bryan/main"
    This reverts commit 21cdab595ba9bf095ef41699bc64ea95a1a26ce6.

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -16,10 +16,12 @@
         <div class="main-box">
             <h1 class="display-1">Welcome to Weathrly</h1>
             <p>Your trusted source for weather updates.</p>
+            @guest
             <div class="container">
                 <a href="/login" class="btn btn-primary" role="button">Login</a>
                 <a href="/register" class="btn btn-secondary" role="button">Register</a>
             </div>
+            @endguest
         </div>
     </div>
     <script src="{{ asset('js/bootstrap.js') }}"></script>
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -4,10 +4,12 @@
             <img src="{{ asset('images/logo.png') }}" alt="Weathrly">
             <span>WEATHRLY</span>
         </a>
+        @auth
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
+            
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('report')}}">Report</a>
@@ -18,14 +20,17 @@
                 <li class="nav-item">
                     <a class="nav-link" href="{{route('heat')}}">Heat</a>
                 </li>
-                <li class="nav-item d-flex flex-row ">
-                    <div class="d-flex flex-column ">
+                <li class="nav-item d-flex flex-row">
+                    <div class="d-flex flex-column">
                         <a class="nav-link pb-0" href="#">
-                            John Doe
+                            {{ Auth::user()->name }}
                         </a>
-                        <form action="{{route('logout')}}" method="post">
+                        <a class="btn btn-outline-danger" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                             @csrf
-                            <button type="submit" class="btn btn-danger btn-sm">Logout</button>
                         </form>
                     </div>
                     <div class="d-flex align-self-center">
@@ -35,6 +40,10 @@
                     </div>
                 </li>
             </ul>
+        @endauth
+            
+        @guest
+        @endguest
         </div>
     </div>
 </nav>
